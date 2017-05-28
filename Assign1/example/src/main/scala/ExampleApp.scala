a90234122139f405d8e0e035d5661be48dbe00b8
Dhananjayan Santhanakrishnan <sdjcse1@gmail.com>
2017-05-28 06:22:29 -0700

    singleton tests

--- Assign1/example/src/main/scala/ExampleApp.scala
+++ Assign1/example/src/main/scala/ExampleApp.scala
@@ -2,7 +2,21 @@
   * Created by sdj on 5/12/17.
   */
 object ExampleApp {
+    private var _instance : ExampleApp = null
+    def instance() = {
+        if (_instance == null)
+            _instance = new ExampleApp(0)
+        _instance
+    }
     def main(args: Array[String]): Unit = {
-        println("Hello World!")
+        ExampleApp.instance.doSomething()
+        ExampleApp.instance.doSomething()
+    }
+}
+
+class ExampleApp private(var state:Int){
+    def doSomething() = {
+        state += 1
+        println("I did something here: " + this.state +" times")
     }
 }
